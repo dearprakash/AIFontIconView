609eb615844912b71d0af676d02ff380c351fb8b
Prakash Rajendran <prakash@applifiedideas.com>
2016-06-11 10:47:31 +0200

    Base

---
+++ AIFontIconView/AIFontIconView.h
@@ -0,0 +1,19 @@
+//
+//  AIFontIconView.h
+//  AIFontIconView
+//
+//  Created by Prakash Rajendran on 10/06/16.
+//  Copyright © 2016 praka.sh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AIFontIconView.
+FOUNDATION_EXPORT double AIFontIconViewVersionNumber;
+
+//! Project version string for AIFontIconView.
+FOUNDATION_EXPORT const unsigned char AIFontIconViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AIFontIconView/PublicHeader.h>
+
+
